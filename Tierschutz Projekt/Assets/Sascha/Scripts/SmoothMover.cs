02b20b398f40c563f8ed22d5916949c866c2a7f1
BossBohne989 <Lukas.hollaender@hotmail.com>
2023-04-01 18:02:45 +0200

    commit

---
+++ Tierschutz Projekt/Assets/Sascha/Scripts/SmoothMover.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothMover : MonoBehaviour
+{
+    [SerializeField] private Transform start;
+    [SerializeField] private Transform startUp;
+    [SerializeField] private Transform startDown;
+    [SerializeField] private Transform middle;
+    [SerializeField] private Transform leftUp;
+    [SerializeField] private Transform left;
+    [SerializeField] private Transform leftDown;
+    [SerializeField] private Transform rightUp;
+    [SerializeField] private Transform right;
+    [SerializeField] private Transform rightDown;
+
+    public bool move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = start.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            move = true; 
+            Debug.Log("move true");
+        }
+
+        if (move == true){
+        transform.position = Vector2.Lerp(transform.position, middle.position, Time.deltaTime);
+        if (middle.position - transform.position <= 0.01f){
+            transform.position = middle.position;
+        }
+        }
+    }
+
+}
